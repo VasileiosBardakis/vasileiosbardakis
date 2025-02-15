a3bb1dccd98500f64b518d28ee6b3c24a294a062
VasileiosBardakis <85302207+VasileiosBardakis@users.noreply.github.com>
2025-02-15 18:18:42 +0200

    done

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-
-
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 const navbar = document.querySelector(".nav-links-bio");
 const navbar2 = document.querySelector(".nav-links-con");
-let lastScrollY=window.scrollY;
-navbar2.classList.add("nav-links-con");
-window.addEventListener("scroll",()=>{
-	if(700<window.scrollY){
+const navbar3 = document.querySelector(".nav-links-pro");
+let lastScrollY = window.scrollY;
+
+navbar.classList.add("nav-links-bio");
+window.addEventListener("scroll", () => {
+	console.log("scrollY", window.scrollY);
+	if (window.scrollY > 869 && window.scrollY < 2469) {
+		
+		navbar.classList.remove("nav-links-bio");
+		navbar2.classList.remove("nav-links-con");
+		navbar3.classList.add("nav-links-pro");
+	} else if (window.scrollY > 2468) {
 		navbar.classList.remove("nav-links-bio");
 		navbar2.classList.add("nav-links-con");
-	}
-	else{
+		navbar3.classList.remove("nav-links-pro");
+	} else {
 		navbar.classList.add("nav-links-bio");
 		navbar2.classList.remove("nav-links-con");
+		navbar3.classList.remove("nav-links-pro");
 	}
 });
 
-window.onload = navbar2.classList.remove("nav-links-con");
+window.onload = () => {
+	navbar2.classList.remove("nav-links-con");
+	navbar3.classList.remove("nav-links-pro");
+};
 
-menu.addEventListener('click',function() {
+menu.addEventListener('click', function() {
 	menu.classList.toggle('is-active');
 	menu.classList.toggle('active');
-
-})
+});
